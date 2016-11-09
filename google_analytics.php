8aaec1af56e2e0a7eb22a251f647c272a5b211e1
Seunghun Geum <shgeum81@gmail.com>
2016-11-09 14:37:00 +0900

    수정: cid (RFC4122) 변경

--- google_analytics.php
+++ google_analytics.php
@@ -1,14 +1,91 @@
 <?php
 define('GOOGLE_ANALYTICS_CODE_CUSTOM','');
+
+class UUID{ //출처: http://php.net/manual/en/function.uniqid.php (작성자: Andrew Moore)
+  public static function RFC4122_v3($namespace, $name) {
+    if(!self::is_valid($namespace)) return false;
+    // Get hexadecimal components of namespace
+    $nhex = str_replace(array('-','{','}'), '', $namespace);
+    // Binary Value
+    $nstr = '';
+    // Convert Namespace UUID to bits
+    for($i = 0; $i < strlen($nhex); $i+=2) {
+      $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+    }
+    // Calculate hash value
+    $hash = md5($nstr . $name);
+    return sprintf('%08s-%04s-%04x-%04x-%12s',
+      // 32 bits for "time_low"
+      substr($hash, 0, 8),
+      // 16 bits for "time_mid"
+      substr($hash, 8, 4),
+      // 16 bits for "time_hi_and_version",
+      // four most significant bits holds version number 3
+      (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
+      // 16 bits, 8 bits for "clk_seq_hi_res",
+      // 8 bits for "clk_seq_low",
+      // two most significant bits holds zero and one for variant DCE1.1
+      (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+      // 48 bits for "node"
+      substr($hash, 20, 12)
+    );
+  }
+  public static function RFC4122_v4() {
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+      // 32 bits for "time_low"
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+      // 16 bits for "time_mid"
+      mt_rand(0, 0xffff),
+      // 16 bits for "time_hi_and_version",
+      // four most significant bits holds version number 4
+      mt_rand(0, 0x0fff) | 0x4000,
+      // 16 bits, 8 bits for "clk_seq_hi_res",
+      // 8 bits for "clk_seq_low",
+      // two most significant bits holds zero and one for variant DCE1.1
+      mt_rand(0, 0x3fff) | 0x8000,
+      // 48 bits for "node"
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+  }
+  public static function RFC4122_v5($namespace, $name) {
+    if(!self::is_valid($namespace)) return false;
+    // Get hexadecimal components of namespace
+    $nhex = str_replace(array('-','{','}'), '', $namespace);
+    // Binary Value
+    $nstr = '';
+    // Convert Namespace UUID to bits
+    for($i = 0; $i < strlen($nhex); $i+=2) {
+      $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+    }
+    // Calculate hash value
+    $hash = sha1($nstr . $name);
+    return sprintf('%08s-%04s-%04x-%04x-%12s',
+      // 32 bits for "time_low"
+      substr($hash, 0, 8),
+      // 16 bits for "time_mid"
+      substr($hash, 8, 4),
+      // 16 bits for "time_hi_and_version",
+      // four most significant bits holds version number 5
+      (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
+      // 16 bits, 8 bits for "clk_seq_hi_res",
+      // 8 bits for "clk_seq_low",
+      // two most significant bits holds zero and one for variant DCE1.1
+      (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+      // 48 bits for "node"
+      substr($hash, 20, 12)
+    );
+  }
+  public static function is_valid($uuid) {
+    return preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
+                      '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', $uuid) === 1;
+  }
+}
 function sendGoogleAnalytics($postData='',$pageTitle='',$uid='',$analyticsCode=_GOOGLE_ANALYTICS_CODE_CUSTOM){
     $requestURL = 'https://www.google-analytics.com/collect';
     $params = array();
     $params[] .= sprintf('v=1');
     $params[] .= sprintf('tid=%s',$analyticsCode);                                                                 //구글 UID
-    if($_COOKIE['PHPSESSID'])
-        $params[] .= sprintf('cid=%s',rawurlencode($_COOKIE['PHPSESSID']));                                        //클라이언트 id(php_self 값으로 대체)
-    else
-        $params[] .= sprintf('cid=%s',str_shuffle('abcdefghijklmlopqrstuvwxyz123456789-'));                        //클라이언트 id(php_self 값으로 대체)
+    $params[] .= sprintf('cid=%s',(isset($_COOKIE['_ga'])?explode('.',$_COOKIE["_ga"],'3')[2]:UUID::RFC4122_v4()));
     if($uid)
         $params[] .= sprintf('uid=%s',rawurlencode($uid));                                                         //사용자id
     $params[] .= sprintf('t=pageview');
